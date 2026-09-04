bbc3d660c23f2ecdbc893485e0ac5393e800cf88
sivashankarm6405 <sivashankarm6405@gmail.com>
2026-09-04 13:48:00 +0000

    Reintroduce bug for repeat test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from self-healing pipeline");
+        System.out.println("Hello from self-healing pipeline")
     }
 }
